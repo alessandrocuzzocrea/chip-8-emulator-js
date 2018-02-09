d3933ab4b5e8cbe81cbf6b1ce7ebbf6e8675eeeb
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-09 19:30:55 +0100

    Refactor chip8.js and chip8.test.js

--- src/chip8.js
+++ src/chip8.js
@@ -10,6 +10,13 @@ function Chip8() {
   this.soundTimer = null;
 }
 
+function cloneDecorator(fn) {
+  return function(chip8) {
+    const newChip8 = _.cloneDeep(chip8);
+    return fn.call(null, newChip8, ...arguments);
+  };
+}
+
 function reset(newChip8) {
   newChip8.memory = Array(4096).fill(0);
   newChip8.v = Array(16).fill(0); //8bit registers
@@ -19,13 +26,6 @@ function reset(newChip8) {
   newChip8.delayTimer = 0;
   newChip8.soundTimer = 0;
   return newChip8;
-}
-
-function cloneDecorator(fn) {
-  return function(chip8) {
-    const newChip8 = _.cloneDeep(chip8);
-    return fn.call(null, newChip8, ...arguments);
-  };
 }
 
 module.exports = {
--- src/chip8.test.js
+++ src/chip8.test.js
@@ -1,39 +1,51 @@
 const chip = require("./chip8");
 
-describe("Initialization", () => {
-  it("has all his members initialized to null", () => {
-    const chip8 = new chip.Chip8();
-    expect(chip8.i).toEqual(null);
-    expect(chip8.memory).toEqual(null);
-    expect(chip8.v).toEqual(null);
-    expect(chip8.i).toEqual(null);
-    expect(chip8.pc).toEqual(null);
-    expect(chip8.sp).toEqual(null);
-    expect(chip8.delayTimer).toEqual(null);
-    expect(chip8.soundTimer).toEqual(null);
+describe("Chip8", () => {
+  describe("new Chip8", () => {
+    it("has all his members initialized to null", () => {
+      const chip8 = new chip.Chip8();
+      expect(chip8.i).toEqual(null);
+      expect(chip8.memory).toEqual(null);
+      expect(chip8.v).toEqual(null);
+      expect(chip8.i).toEqual(null);
+      expect(chip8.pc).toEqual(null);
+      expect(chip8.sp).toEqual(null);
+      expect(chip8.delayTimer).toEqual(null);
+      expect(chip8.soundTimer).toEqual(null);
+    });
   });
 
-  it("is correctly initialized after reset", () => {
-    let chip8 = new chip.Chip8();
-    chip8 = chip.reset(chip8);
+  describe("reset", () => {
+    it("is correctly initialized after reset", () => {
+      let chip8 = new chip.Chip8();
+      chip8 = chip.reset(chip8);
 
-    //Memory
-    expect(chip8.memory).toBeInstanceOf(Array);
-    expect(chip8.memory).toHaveLength(4096);
-    expect(chip8.memory.reduce((acc, v) => acc + v)).toEqual(0);
+      //Memory
+      expect(chip8.memory).toBeInstanceOf(Array);
+      expect(chip8.memory).toHaveLength(4096);
+      expect(chip8.memory.reduce((acc, v) => acc + v)).toEqual(0);
 
-    //v registers
-    expect(chip8.v).toBeInstanceOf(Array);
-    expect(chip8.v).toHaveLength(16);
-    expect(chip8.v.reduce((acc, v) => acc + v)).toEqual(0);
+      //v registers
+      expect(chip8.v).toBeInstanceOf(Array);
+      expect(chip8.v).toHaveLength(16);
+      expect(chip8.v.reduce((acc, v) => acc + v)).toEqual(0);
 
-    //i register
-    expect(chip8.i).toEqual(0);
+      //i register
+      expect(chip8.i).toEqual(0);
 
-    expect(chip8.pc).toEqual(0x200);
-    expect(chip8.sp).toEqual(0x0);
+      expect(chip8.pc).toEqual(0x200);
+      expect(chip8.sp).toEqual(0x0);
 
-    expect(chip8.delayTimer).toEqual(0);
-    expect(chip8.soundTimer).toEqual(0);
+      expect(chip8.delayTimer).toEqual(0);
+      expect(chip8.soundTimer).toEqual(0);
+    });
+  });
+
+  describe("loadCharset", () => {
+    it("load the charset", () => {
+      let chip8 = new chip.Chip8();
+      chip8 = chip.reset(chip8);
+      chip8 = chip.loadCharset(chip8);
+    });
   });
 });
