79cc74d11133b71972b1a8596cdbd57b7d79d8c6
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-03-01 21:08:13 +0100

    Change renderer colors

--- src/renderer.js
+++ src/renderer.js
@@ -4,8 +4,10 @@ const SCALE = 8;
 
 function render(chip, canvas) {
   const ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgb(0, 0, 0)";
+  ctx.fillStyle = "#1abc9c";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#2c3e50";
 
   for (let y = 0; y < c.screenHeight; y++) {
     for (let x = 0; x < c.screenWidth; x++) {
