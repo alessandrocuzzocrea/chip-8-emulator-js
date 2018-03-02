343179cee83e8c45525b48618957bf438cc9b6c6
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-03-02 19:23:08 +0100

    Add better html and css handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 const emulator = require("./emulator");
 
 emulator.init();
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = env => {
   let GHPAGES = false;
@@ -13,18 +14,27 @@ module.exports = env => {
       path: __dirname + "/dist",
       filename: "bundle.js"
     },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: "css-loader"
+          })
+        }
+      ]
+    },
     plugins: [
       new webpack.DefinePlugin({
         "process.env": {
           GHPAGES: JSON.stringify(GHPAGES)
         }
       }),
-      new HtmlWebpackPlugin({ template: __dirname + "/public/index.html" }),
-      new CopyWebpackPlugin([
-        { from: __dirname + "/roms", to: "./roms" },
-        { from: __dirname + "/public/style.css", to: "./style.css" }
-      ]),
-      new CleanWebpackPlugin([__dirname + "/dist"])
+      new HtmlWebpackPlugin({ template: "index.html" }),
+      new CopyWebpackPlugin([{ from: __dirname + "/roms", to: "./roms" }]),
+      new CleanWebpackPlugin([__dirname + "/dist"]),
+      new ExtractTextPlugin("styles.css")
     ],
     devtool: "source-map"
   };
